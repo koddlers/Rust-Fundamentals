332cccad0b65f696b52675c959a5766503b38494
shaphil <shaphil2025@gmail.com>
2024-04-29 17:17:00 +0600

    Add Module 04
    Rust Fundamentals
      * Module 04 - Variables
        * Lesson 02 - Variables
        * Lesson 03 - Casting Data Types
        * Lesson 04 - Variable Mutability
        * Lesson 05 - Scope and Shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod module_03;
+mod module_04;
 
 pub use module_03::data_types;
+pub use module_04::variables;
 
 fn main() {
     // 03 - Data Types -> 02 - Number Types
@@ -8,5 +10,9 @@ fn main() {
     // data_types::bool_and_char();
     // data_types::arrays_and_tuples();
     // data_types::strings_and_string_slices();
-    data_types::string_concatenation();
+    // data_types::string_concatenation();
+    // variables::vars();
+    // variables::casting_data_types();
+    // variables::variable_mutability();
+    variables::scope_and_shadowing();
 }
